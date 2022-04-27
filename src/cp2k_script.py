45852c1ba4789bb4675ce0c5252465568d49dfde
Ke Xiong <77319966+appassionate@users.noreply.github.com>
2022-04-27 11:23:35 +0800

    Update cp2k_script.py

--- src/cp2k_script.py
+++ src/cp2k_script.py
@@ -1,6 +1,6 @@
 from .run import *
 
-run =RunCP2K("/home/kxiong/trash","trash","abc",dummy_make_func, {"1":"2"})
+run =RunCP2K("/home/username/trash","trash","abc",dummy_make_func, {"1":"2"})
 
 run._render_make_func(structure="struct", cell=[1,2,3], basis_set=[1,2,],calc_type="cp2k")
 
